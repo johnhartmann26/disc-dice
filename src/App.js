87faac55905c3a2a9b76bf3390a0ca1b303edede
John Hartmann <johnhartmann26@gmail.com>
2021-04-22 22:06:12 -0500

    init functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shotTypes: [
+        "backhand hyzer",
+        "forehand hyzer",
+        "backhand anhyzer",
+        "forehand anhyzer",
+        "backhand roller",
+        "forehand roller",
+        "tomahawk",
+        "grenade",
+        "thumber"
+      ],
+      discTypes: [
+        "understable putter",
+        "overstable putter",
+        "straight putter",
+        "understable mid",
+        "overstable mid",
+        "straight mid",
+        "understable driver",
+        "overstable driver",
+        "straight driver"
+      ],
+      disc: "disc",
+      shot: "shot"
+    };
+  }
+  // Math.floor(Math.random() * this.state.shotTypes.length)
+
+  handleClick = () => {
+    this.setState({
+      disc: this.state.discTypes[Math.floor(Math.random() * this.state.discTypes.length)],
+      shot: this.state.shotTypes[Math.floor(Math.random() * this.state.shotTypes.length)]
+    })
+}
+
+
+
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Disc Dice</h1>
+        <button onClick={this.handleClick}>roll</button>
+        <h2>Throw this disc: {this.state.disc}</h2>
+        <h2>On this angle: {this.state.shot}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
